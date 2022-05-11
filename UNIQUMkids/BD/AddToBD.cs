a976f84fd47f84db0dc42bc96b5dad91e15b5065
leeeeeeeeeemon <emilbarish@gmail.com>
2022-05-11 08:23:17 +0300

    Добавьте файлы проекта.

---
+++ UNIQUMkids/BD/AddToBD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIQUMkids.BD
+{
+    public  class AddToBD
+    {
+        public void AddChild(Child child)
+        {
+            bd_connection.connection.Child.Add(child);
+            bd_connection.connection.SaveChanges();
+        }
+
+        public void AddLessonChild(LessonChild lessonChild)
+        {
+            bd_connection.connection.LessonChild.Add(lessonChild);
+            bd_connection.connection.SaveChanges();
+        }
+
+        public void AddParent(Parent parent)
+        {
+            bd_connection.connection.Parent.Add(parent);
+            bd_connection.connection.SaveChanges();
+        }
+
+        public void AddTeacher(Teacher teacher)
+        {
+            bd_connection.connection.Teacher.Add(teacher);
+            bd_connection.connection.SaveChanges();
+        }
+    }
+}
---
+++ UNIQUMkids/BD/GetDataBD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UNIQUMkids.BD;
+namespace UNIQUMkids.BD
+{
+    public class GetDataBD
+    {
+        public ObservableCollection<Child> GetChild()
+        {
+            return new ObservableCollection<Child>(bd_connection.connection.Child.ToList());
+        }
+
+        public ObservableCollection<Parent> GetParent()
+        {
+            return new ObservableCollection<Parent>(bd_connection.connection.Parent.ToList());
+        }
+
+        public ObservableCollection<Gender> GetGender()
+        {
+            return new ObservableCollection<Gender>(bd_connection.connection.Gender.ToList());
+        }
+
+        public ObservableCollection<Lesson> GetLesson()
+        {
+            return new ObservableCollection<Lesson>(bd_connection.connection.Lesson.ToList());
+        }
+
+        public ObservableCollection<LessonChild> GetLessonChild()
+        {
+            return new ObservableCollection<LessonChild>(bd_connection.connection.LessonChild.ToList());
+        }
+
+        public ObservableCollection<Raspisanie> GetRaspisanie()
+        {
+            return new ObservableCollection<Raspisanie>(bd_connection.connection.Raspisanie.ToList());
+        }
+    }
+}
